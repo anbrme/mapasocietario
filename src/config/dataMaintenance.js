6f028b008b07f1c88a5b95379a2e3d807e019dcd
anbrme <61997613+anbrme@users.noreply.github.com>
2026-06-30 13:33:35 +0200

    maintenance mode to false

--- src/config/dataMaintenance.js
+++ src/config/dataMaintenance.js
@@ -1,5 +1,5 @@
 export const DATA_MAINTENANCE = Object.freeze({
-  enabled: true,
+  enabled: false,
   title: 'Index fix in progress to target a misspelling (a whitespace in the company\'s suffix) in the BORME source documents which was not being handled correctly. As a result, companies acting as officers acould see the suffix wrongly displayed in the search results and in the graph. Fixed, re-indexing and backfill are in progress.',
   message:
     'Company and officer search remains available while we upgrade the index in the background. Some results may be incomplete or briefly delayed during the upgrade.',
